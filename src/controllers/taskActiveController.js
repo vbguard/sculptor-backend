f5b1f9696069d4c662c41fc71161838e8006b763
Viktor Bulvarenko <silken.ua@gmail.com>
2019-06-19 12:47:18 +0300

    fix delete activeDay response and some logivc

--- src/controllers/taskActiveController.js
+++ src/controllers/taskActiveController.js
@@ -1,31 +1,46 @@
 const Task = require("../models/taskModel.js");
 
-module.exports.deleteTaskActiveDay = (req, res) => {
+module.exports.deleteTaskActiveDay = async (req, res) => {
   const taskId = req.params.taskId;
   const taskActiveDayId = req.body.taskActiveDayId;
+  try {
+    const updateTask = await Task.findOneAndUpdate(
+      { _id: taskId },
+      {
+        $pull: {
+          taskActiveDates: { _id: taskActiveDayId }
+        }
+      },
+      { multi: true, new: true }
+    );
 
-  Task.update(
-    { _id: taskId },
-    {
-      $pull: {
-        taskActiveDates: { _id: taskActiveDayId }
-      }
-    },
-    { multi: true },
-    (err, updatedTask) => {
-      if (err) {
-        es.status(404).json({
-          success: false,
-          message: err.message
-        });
-      }
+    const setTaskIsComplete = updateTask.taskActiveDates.filter(
+      day => !day.isDone
+    );
 
+    if (setTaskIsComplete.length > 0) {
+      updateTask.isComplete = false;
+      updateTask.save();
+    }
+
+    if (setTaskIsComplete.length === 0) {
+      updateTask.isComplete = true;
+      updateTask.save();
+    }
+
+    if (updateTask) {
       res.status(201).json({
-        message: "TaskActive success remove, Task updated",
-        updatedTask: updatedTask
+        success: true,
+        message: "TaskActive success updated, Task updated",
+        updatedTask: updateTask
       });
     }
-  );
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message
+    });
+  }
 };
 
 module.exports.changeStatusTaskActiveDay = async (req, res) => {
